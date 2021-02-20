d76b25776859fcd87ad3991bbb5dedf502924ecd
rito15 <ghdtkals15@gmail.com>
2021-02-21 00:22:12 +0900

    [2021-02-21] update

---
+++ Unity Library/Static/TestTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 날짜 : #DATE#
+// 작성자 : Rito
+
+namespace Rito
+{
+    public class TestTest : MonoBehaviour
+    {
+        
+    }
+}
